b71c099a2fc548ad34c63e8cc9eda726e1cf4306
Steve Rogers <srogers.design@gmail.com>
2014-12-12 17:28:08 -0500

    initial commit

---
+++ game.rb
@@ -0,0 +1,41 @@
+require 'gosu'
+
+require_relative 'models/player'
+
+class GameWindow < Gosu::Window
+  def initialize
+    super(800, 600, false)
+    self.caption = "Gosu Tutorial Game"
+    @background_image = Gosu::Image.new(self, "img/ppt.png", true)
+
+    @player = Player.new(self)
+    @player.warp(320, 240)
+  end
+
+  def update
+    if button_down? Gosu::KbLeft or button_down? Gosu::GpLeft then
+      @player.turn_left
+    end
+    if button_down? Gosu::KbRight or button_down? Gosu::GpRight then
+      @player.turn_right
+    end
+    if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
+      @player.accelerate
+    end
+    @player.move
+  end
+
+  def draw
+    @player.draw
+    @background_image.draw(0, 0, 0);
+  end
+
+  def button_down(id)
+    if id == Gosu::KbEscape
+      close
+    end
+  end
+end
+
+window = GameWindow.new
+window.show
